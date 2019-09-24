4aff777d157a8550dc48fd2562fb70d00cb792ff
David Stewart <davidstewart@gmail.com>
2019-09-24 02:54:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+
+
+function writeCards(names,event) {
+ let  giftarray = []
+  for (let i = 0; i < names.length; i++)  {
+    giftarray.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+  }
+ return  giftarray
+}
+
+
+function countdown(number) {
+  let num = 0
+  while (num <= number){
+    console.log(`${number - num}`)
+    num++
+  }
+  
+}
